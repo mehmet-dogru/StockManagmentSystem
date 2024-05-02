7e04697533df74369a360b2fb0f1b1e2d46c24ac
mehmet-dogru <mdogru685@gmail.com>
2024-05-02 12:22:10 +0300

    feat: implemented graceful shutdown on server

--- internal/api/server.go
+++ internal/api/server.go
@@ -6,9 +6,12 @@ import (
 	"DynamicStockManagmentSystem/internal/api/rest"
 	"DynamicStockManagmentSystem/internal/api/rest/handlers"
 	"DynamicStockManagmentSystem/internal/helper"
+	"fmt"
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"log"
+	"os"
+	"os/signal"
 )
 
 func StartServer(config config.AppConfig) {
@@ -28,7 +31,19 @@ func StartServer(config config.AppConfig) {
 
 	setupRoutes(rh)
 
-	log.Fatal(app.Listen(config.ServerPort))
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		_ = <-c
+		fmt.Println("gracefully shutting down")
+		_ = app.Shutdown()
+	}()
+
+	if err := app.Listen(config.ServerPort); err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println("running cleanup tasks...")
 }
 
 func setupRoutes(rh *rest.RestHandler) {
